fbe20c0469c93de07c673314180f74423d6ff4a6
Mo! <mhd.murad.87@gmail.com>
2023-11-06 20:47:40 +0300

    Update CommentAchievementsTest.php
    Expanded the test suite with additional cases, to cover AchievementUnlocked event dispatched when comment written.

--- tests/Feature/CommentAchievementsTest.php
+++ tests/Feature/CommentAchievementsTest.php
@@ -2,16 +2,20 @@
 
 namespace Tests\Feature;
 
+use App\Events\AchievementUnlocked;
 use App\Events\CommentWritten;
 use App\Models\Achievement;
 use App\Models\Comment;
 use App\Models\User;
+use Database\Seeders\AchievementSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Event;
 use Tests\TestCase;
 
 class CommentAchievementsTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic feature test a user can unlock first comment written achievement.
      *
@@ -118,5 +122,45 @@ class CommentAchievementsTest extends TestCase
             event(new CommentWritten($comment));
         }
         $this->assertTrue($user->achievements->contains('name', $achievementName), "Failed to assert that the '{$achievementName}' achievement was unlocked.");
+    }
+
+    /**
+     * A basic feature test a comment achievement unlocked dispatched.
+     *
+     * @return void
+     */
+    public function test_a_comment_achievement_unlocked_dispatched()
+    {
+        $this->seed(AchievementSeeder::class);
+        $commentCount = 1;
+        Event::fake(AchievementUnlocked::class);
+        $user = User::factory()->create();
+        for ($i = 0; $i < $commentCount; $i++) {
+            $comment = Comment::factory()->create(['user_id' => $user->id]);
+            event(new CommentWritten($comment));
+        }
+        Event::assertDispatched(AchievementUnlocked::class, function ($event) use ($user) {
+            return $event->user->is($user);
+        });
+
+    }
+
+    /**
+     * A basic feature test a comment achievement unlocked dispatched n of times.
+     *
+     * @return void
+     */
+    public function test_a_comment_achievement_unlocked_dispatched_n_of_times()
+    {
+        $this->seed(AchievementSeeder::class);
+        $commentCount = 20;
+        $achievementCount = 5;
+        Event::fake(AchievementUnlocked::class);
+        $user = User::factory()->create();
+        for ($i = 0; $i < $commentCount; $i++) {
+            $comment = Comment::factory()->create(['user_id' => $user->id]);
+            event(new CommentWritten($comment));
+        }
+        Event::assertDispatchedTimes(AchievementUnlocked::class, $achievementCount);
     }
 }
