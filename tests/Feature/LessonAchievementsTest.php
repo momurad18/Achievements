91238f4a58bf754fe2cd3791eba3e699a514c2be
Mo! <mhd.murad.87@gmail.com>
2023-11-06 20:10:15 +0300

    Update LessonAchievementsTest.php
    Expanded the test suite with additional cases, each focusing on a specific achievement.

--- tests/Feature/LessonAchievementsTest.php
+++ tests/Feature/LessonAchievementsTest.php
@@ -21,16 +21,117 @@ class LessonAchievementsTest extends TestCase
      */
     public function test_a_user_can_unlock_first_lesson_achievement(): void
     {
+        $lessonCount = 1;
         $achievementName = 'First Lesson Watched';
         $user = User::factory()->create();
         $lesson = Lesson::factory()->create();
-        $user->lessons()->attach($lesson->id, ['watched' => 1]);
         $achievement = Achievement::factory()->create([
             'name' => $achievementName,
-            'required_count' => 1,
+            'required_count' => $lessonCount,
             'type' => 'lesson',
         ]);
+        $user->lessons()->attach($lesson->id, ['watched' => 1]);
         event(new LessonWatched($lesson, $user));
         $this->assertTrue($user->achievements->contains('name', $achievementName), "Failed to assert that the '{$achievementName}' achievement was unlocked.");
     }
+
+     /**
+     * A basic feature test a user can unlock 5 lessons achievement.
+     *
+     * @return void
+     */
+    public function test_a_user_can_unlock_5_lessons_achievement(): void
+    {
+        $lessonCount = 5;
+        $achievementName = "{$lessonCount} Lessons Watched";
+        $user = User::factory()->create();
+        $lessons = Lesson::factory()
+            ->count($lessonCount)
+            ->create();
+        $achievement = Achievement::factory()->create([
+            'name' => $achievementName,
+            'required_count' => $lessonCount,
+            'type' => 'lesson',
+        ]);
+        foreach($lessons as $lesson){
+            $user->lessons()->attach($lesson->id, ['watched' => 1]);
+            event(new LessonWatched($lesson, $user));
+        }
+        $this->assertTrue($user->achievements->contains('name', $achievementName), "Failed to assert that the '{$achievementName}' achievement was unlocked.");
+    }
+
+    /**
+     * A basic feature test a user can unlock 10 lessons achievement.
+     *
+     * @return void
+     */
+    public function test_a_user_can_unlock_10_lessons_achievement(): void
+    {
+        $lessonCount = 10;
+        $achievementName = "{$lessonCount} Lessons Watched";
+        $user = User::factory()->create();
+        $lessons = Lesson::factory()
+            ->count($lessonCount)
+            ->create();
+        $achievement = Achievement::factory()->create([
+            'name' => $achievementName,
+            'required_count' => $lessonCount,
+            'type' => 'lesson',
+        ]);
+        foreach($lessons as $lesson){
+            $user->lessons()->attach($lesson->id, ['watched' => 1]);
+            event(new LessonWatched($lesson, $user));
+        }
+        $this->assertTrue($user->achievements->contains('name', $achievementName), "Failed to assert that the '{$achievementName}' achievement was unlocked.");
+    }
+    /**
+     * A basic feature test a user can unlock 25 lessons achievement.
+     *
+     * @return void
+     */
+    public function test_a_user_can_unlock_25_lessons_achievement(): void
+    {
+        $lessonCount = 25;
+        $achievementName = "{$lessonCount} Lessons Watched";
+        $user = User::factory()->create();
+        $lessons = Lesson::factory()
+            ->count($lessonCount)
+            ->create();
+        $achievement = Achievement::factory()->create([
+            'name' => $achievementName,
+            'required_count' => $lessonCount,
+            'type' => 'lesson',
+        ]);
+        foreach($lessons as $lesson){
+            $user->lessons()->attach($lesson->id, ['watched' => 1]);
+            event(new LessonWatched($lesson, $user));
+        }
+        $this->assertTrue($user->achievements->contains('name', $achievementName), "Failed to assert that the '{$achievementName}' achievement was unlocked.");
+    }
+
+    /**
+     * A basic feature test a user can unlock 50 lessons achievement.
+     *
+     * @return void
+     */
+    public function test_a_user_can_unlock_50_lessons_achievement(): void
+    {
+        $lessonCount = 50;
+        $achievementName = "{$lessonCount} Lessons Watched";
+        $user = User::factory()->create();
+        $lessons = Lesson::factory()
+            ->count($lessonCount)
+            ->create();
+        $achievement = Achievement::factory()->create([
+            'name' => $achievementName,
+            'required_count' => $lessonCount,
+            'type' => 'lesson',
+        ]);
+        foreach($lessons as $lesson){
+            $user->lessons()->attach($lesson->id, ['watched' => 1]);
+            event(new LessonWatched($lesson, $user));
+        }
+        $this->assertTrue($user->achievements->contains('name', $achievementName), "Failed to assert that the '{$achievementName}' achievement was unlocked.");
+    }
+
 }
