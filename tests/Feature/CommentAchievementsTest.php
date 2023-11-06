54bbb383b155c9e0754b352fbe78528321931e3a
Mo! <mhd.murad.87@gmail.com>
2023-11-06 20:38:54 +0300

    Update CommentAchievementsTest.php
    Expanded the test suite with additional cases, each focusing on a specific achievement.

--- tests/Feature/CommentAchievementsTest.php
+++ tests/Feature/CommentAchievementsTest.php
@@ -31,4 +31,92 @@ class CommentAchievementsTest extends TestCase
         event(new CommentWritten($comment));
         $this->assertTrue($user->achievements->contains('name', $achievementName), "Failed to assert that the '{$achievementName}' achievement was unlocked.");
     }
+
+    /**
+     * A basic feature test a user can unlock 3 comments written achievement.
+     *
+     * @return void
+     */
+    public function test_a_user_can_unlock_3_comments_written(): void
+    {
+        $commentCount = 3;
+        $achievementName = "{$commentCount} Comments Written";
+        $user = User::factory()->create();
+        $achievement = Achievement::factory()->create([
+            'name' => $achievementName,
+            'required_count' => $commentCount,
+            'type' => 'comment',
+        ]);
+        for ($i = 0; $i < $commentCount; $i++) {
+            $comment = Comment::factory()->create(['user_id' => $user->id]);
+            event(new CommentWritten($comment));
+        }
+        $this->assertTrue($user->achievements->contains('name', $achievementName), "Failed to assert that the '{$achievementName}' achievement was unlocked.");
+    }
+
+    /**
+     * A basic feature test a user can unlock 5 comments written achievement.
+     *
+     * @return void
+     */
+    public function test_a_user_can_unlock_5_comments_written(): void
+    {
+        $commentCount = 5;
+        $achievementName = "{$commentCount} Comments Written";
+        $user = User::factory()->create();
+        $achievement = Achievement::factory()->create([
+            'name' => $achievementName,
+            'required_count' => $commentCount,
+            'type' => 'comment',
+        ]);
+        for ($i = 0; $i < $commentCount; $i++) {
+            $comment = Comment::factory()->create(['user_id' => $user->id]);
+            event(new CommentWritten($comment));
+        }
+        $this->assertTrue($user->achievements->contains('name', $achievementName), "Failed to assert that the '{$achievementName}' achievement was unlocked.");
+    }
+
+     /**
+     * A basic feature test a user can unlock 10 comments written achievement.
+     *
+     * @return void
+     */
+    public function test_a_user_can_unlock_10_comments_written(): void
+    {
+        $commentCount = 10;
+        $achievementName = "{$commentCount} Comments Written";
+        $user = User::factory()->create();
+        $achievement = Achievement::factory()->create([
+            'name' => $achievementName,
+            'required_count' => $commentCount,
+            'type' => 'comment',
+        ]);
+        for ($i = 0; $i < $commentCount; $i++) {
+            $comment = Comment::factory()->create(['user_id' => $user->id]);
+            event(new CommentWritten($comment));
+        }
+        $this->assertTrue($user->achievements->contains('name', $achievementName), "Failed to assert that the '{$achievementName}' achievement was unlocked.");
+    }
+
+     /**
+     * A basic feature test a user can unlock 20 comments written achievement.
+     *
+     * @return void
+     */
+    public function test_a_user_can_unlock_20_comments_written(): void
+    {
+        $commentCount = 20;
+        $achievementName = "{$commentCount} Comments Written";
+        $user = User::factory()->create();
+        $achievement = Achievement::factory()->create([
+            'name' => $achievementName,
+            'required_count' => $commentCount,
+            'type' => 'comment',
+        ]);
+        for ($i = 0; $i < $commentCount; $i++) {
+            $comment = Comment::factory()->create(['user_id' => $user->id]);
+            event(new CommentWritten($comment));
+        }
+        $this->assertTrue($user->achievements->contains('name', $achievementName), "Failed to assert that the '{$achievementName}' achievement was unlocked.");
+    }
 }
